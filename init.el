5979137bc9bebc6391f7383e6df435cd9908e5e2
Mel Collins <mel.collins@ardoq.com>
2018-10-11 15:25:26 +0200

    Add projectile config

--- init.el
+++ init.el
@@ -1,11 +1,5 @@
 (package-initialize)
 
-;; Add line numbers on all files
-;; The v26+ mode is apparently more efficient
-(if (version<= "26.0.50" emacs-version)
-  (global-display-line-numbers-mode)
-  (global-linum-mode))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,3 +12,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Add line numbers on all files
+;; The v26+ mode is apparently more efficient
+(if (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode)
+  (global-linum-mode))
+
+;; Fire off projectiles
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
