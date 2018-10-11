1579fecc099661e12b2631949f16379fc9924823
Mel Collins <mel.collins@ardoq.com>
2018-10-11 15:57:14 +0200

    Add melpa, because someone on the internet said to

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 (package-initialize)
 
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
