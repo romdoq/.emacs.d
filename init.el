8ca9390bbc41c7dbcd3f1e2b4f430a6916081dcc
Mel Collins <mel.collins@ardoq.com>
2018-10-11 14:47:15 +0200

    Add line numbers to all files

---
+++ init.el
@@ -0,0 +1,6 @@
+;; Add line numbers on all files
+;; The v26+ mode is apparently more efficient
+(if (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode)
+  (global-linum-mode))
+
